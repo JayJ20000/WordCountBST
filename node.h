cda818f39fc27614ae676ac58c500d881ce015af
JayJ20000 <111758412+JayJ20000@users.noreply.github.com>
2025-04-12 23:56:52 -0500

    Updated node.h

--- node.h
+++ node.h
@@ -1,26 +1,34 @@
-#ifndef NODE_H
-#define NODE_H
+/**
+ * @file node.h
+ * @brief Node class used in the BST
+ * 
+ * @author Jalen Jones
+ * @date 4-10-2025
+ */
 
-#include <string>
-using namespace std;
-
-// Node class represents each node in the tree
-class Node {
-public:
-    // Constructor
-    Node(string key, int value);
-
-    // The key associated with the node
-    string key;
-
-    // The data/value associated with the key
-    int data;
-
-    // Pointer to the left child
-    Node *left;
-
-    // Pointer to the right child
-    Node *right;
-};
-
-#endif // NODE_H
+ #ifndef NODE_H
+ #define NODE_H
+ 
+ #include <string>
+ using namespace std;
+ 
+ /**
+  * @class Node
+  * @brief Represents each node
+  */
+ class Node {
+ public:
+     /**
+      * @brief Constructor
+      * @param key The key associated with the node
+      * @param value The integer value associated with the key
+      */
+     Node(string key, int value);
+ 
+     string key;    // The key associated with the node
+     int data;      // The value associated with the key
+     Node* left;    // Pointer to the left child
+     Node* right;   // Pointer to the right child
+ };
+ 
+ #endif // NODE_H 
